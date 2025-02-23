a266b4827a3811aae0d74c0b8ceb64165069c13f
nmaju <nmajutee@gmail.com>
2025-02-23 17:02:46 +0100

    0. A Magical Database for Your Dream Online Bookstore Adventure!

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -35,7 +35,7 @@ CREATE TABLE Orders (
 )
 
 -- Create Order Details Table
-CREATE TABLE Orders_Details (
+CREATE TABLE Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
