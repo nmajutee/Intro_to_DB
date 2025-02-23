f6d0d63ed390c380e9ad60c999d08b72eb6c2257
nmaju <nmajutee@gmail.com>
2025-02-23 19:57:03 +0100

    6. Populating Your Database with Your More Data

---
+++ task_6.sql
@@ -0,0 +1,2 @@
+INSERT INTO customer (Ccustomer_id, customer_name, email, address)
+VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'), (3, 'Obed Ehoneah', 'obed@sandtech.com', '125 Happiness  Ave.'), (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
